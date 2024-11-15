92f325e6025c768dc36da36d08bbf72a9626d11e
Novice000 <maxwellefemena@gmail.com>
2024-11-15 23:16:43 +0100

    finished view functions

--- network/models.py
+++ network/models.py
@@ -7,14 +7,22 @@ class User(AbstractUser):
 
 class Followers(models.Model):
     class Meta:
-        verbose_nam = "Follower"
+        verbose_name = "Follower"
         verbose_name_plural = "Followers"
         
-    user = models.ForeignKeyField(User, on_delete = models.CASCADE, related_name = "follower")
-    follower = models.ForeignKeyField(User, on_delete = models.CASCADE, related_name="following")
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "followers")
+    follower = models.ForeignKey(User, on_delete = models.CASCADE, related_name="following")
     
     def __str__(self) -> str:
         return f"{self.user} follows {self.follower}"
+    
+class Likes(models.Model):
+    class Meta:
+        verbose_name = "Like"
+        verbose_name_plural = "Likes"
+        
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "likes")
+    fan = models.ForeignKey(User, on_delete = models.CASCADE, related_name="liker")
 
 class Posts(models.Model):
     class Meta:
@@ -23,7 +31,7 @@ class Posts(models.Model):
         
     user = models.ForeignKeyField(User, on_delete= models.CASCADE, related_name="posts")
     post_title = models.CharField(max_length=30)
-    post = models.CharFieldKey(max_length = 400)
+    post = models.CharField(max_length = 400)
     likes = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
--- network/views.py
+++ network/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
-from .models import User, Posts
+from .models import Followers, User, Posts, Likes
 
 
 def index(request) -> HttpResponse:
@@ -64,6 +65,7 @@ def register(request) -> HttpResponse | HttpResponseRedirect:
     else:
         return render(request, "network/register.html")
 
+@login_required
 def posts(request) -> HttpResponse:
     posts = Posts.objects.all()
     return render("network/index.html", {
@@ -77,14 +79,58 @@ def create(request):
         if form.is_valid():
             form.save()
     elif request.method == "GET":
-        return redirect("index")
+        return redirect(reverse("index"))
     
 def update(request, id):
     if request.method == "POST":
         post = Posts.objects.get(pk=int(id))
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-    elif request.method == "GET":
-        return redirect("index")
+        if post.user == request.user:
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                form.save()
+        elif request.method == "GET":
+            return redirect(reverse("index"))   
+    
+def profile(request):
+    if not request.method == "GET":
+        return redirect(reverse("index"))
+    
+    id  = request.GET.get("id", None)
+    if id is None:
+        return redirect(reverse("index"))
+    
+    try:
+        user = User.objects.prefetch_related("posts","followers").get(id = id)
+    except User.DoesNotExist:
+        return redirect(reverse("index"))
+    
+    posts = Paginator(user.posts.all().order_by("-created_at"),10)
+    followers = user.followers.all()
+    
+    context = {
+        "user": user,
+        "posts": posts,
+        "followers": followers
+    }
+    
+    return render( "", context)
+
+def like_unlike(request):
+    post_id = request.GET.get("id", None)
+    action = request.GET.get("action", None)
+    
+    if post_id == None or action == None:
+        return JsonResponse({
+            "error": "Post id not found or Invalid action"
+        }, 404)
         
+    else:
+        post = Posts.object.get(pk=int(id))
+        post.like += 1
+        likes = Likes.objects.create(user = post.user, fan = request.user)
+        post.save()
+        likes.save()
+        return JsonResponse({
+            "like_count": post.likes,
+            "already_liked": True
+        })
